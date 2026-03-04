e938fff59ee6f10fdfec800d3b53aa225724cbeb
jmc <33655003+jmcook1186@users.noreply.github.com>
2026-03-04 13:39:58 +0000

    add run_emulator wrapper

--- biosnicar/__init__.py
+++ biosnicar/__init__.py
@@ -13,6 +13,13 @@ def run_model(*args, **kwargs):
     return _run_model(*args, **kwargs)
 
 
+def run_emulator(*args, **kwargs):
+    """Evaluate a trained emulator. See :func:`biosnicar.drivers.run_emulator.run_emulator`."""
+    from biosnicar.drivers.run_emulator import run_emulator as _run_emulator
+
+    return _run_emulator(*args, **kwargs)
+
+
 def to_platform(*args, **kwargs):
     """Convolve spectral albedo onto platform bands. See :func:`biosnicar.bands.to_platform`."""
     from biosnicar.bands import to_platform as _to_platform
